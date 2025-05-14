2ad0fc43bd817995294d81d274b8b928bf8641fd
Prokhor <prohorguletski@gmail.com>
2025-05-14 18:29:47 +0300

    gitignore add

--- Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df067ee19291519791edb487d6b664c970041215")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+890c347b74eae0d5303c9efd169694ea3dd91c0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
